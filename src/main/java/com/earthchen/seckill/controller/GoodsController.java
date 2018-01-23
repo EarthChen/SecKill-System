682844f7e0574ff8309e4613f610cd7e761c81c7
EarthChen <179153809@qq.com>
2018-01-23 15:15:12 +0800

    商品详情

--- src/main/java/com/earthchen/seckill/controller/GoodsController.java
+++ src/main/java/com/earthchen/seckill/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.earthchen.seckill.controller;
 
 
 import com.earthchen.seckill.domain.SecKillUser;
+import com.earthchen.seckill.domain.User;
 import com.earthchen.seckill.redis.RedisService;
 import com.earthchen.seckill.service.GoodsService;
 import com.earthchen.seckill.service.SecKillUserService;
@@ -9,6 +10,8 @@ import com.earthchen.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,7 +30,7 @@ public class GoodsController {
     private GoodsService goodsService;
 
 
-    @RequestMapping("/to_list")
+    @GetMapping("/to_list")
     public String list(Model model,
                        SecKillUser user) {
         List<GoodsVo> goodsVoList = goodsService.listGoodsVo();
@@ -35,6 +38,35 @@ public class GoodsController {
         model.addAttribute("user", user);
         model.addAttribute("goodsList", goodsVoList);
         return "goods_list";
+    }
+
+    @GetMapping("/to_detail/{goodsId}")
+    public String detail(Model model, User user,
+                         @PathVariable("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if (now < startAt) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) {//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        return "goods_detail";
     }
 
 }
