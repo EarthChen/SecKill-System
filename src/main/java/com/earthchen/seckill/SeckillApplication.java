8f2b65b84d5086bf2f71cebe1aafe1e0db069e8c
EarthChen <179153809@qq.com>
2018-01-23 17:34:05 +0800

    打war包

--- src/main/java/com/earthchen/seckill/SeckillApplication.java
+++ src/main/java/com/earthchen/seckill/SeckillApplication.java
@@ -3,12 +3,20 @@ package com.earthchen.seckill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.earthchen.seckill.dao")
-public class SeckillApplication {
+public class SeckillApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SeckillApplication.class, args);
+    }
+
+    //    打war包需要
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SeckillApplication.class);
     }
 }
