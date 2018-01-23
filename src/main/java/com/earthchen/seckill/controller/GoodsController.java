bb7491ebb618c7e2ada769a1a18f159395ad43b9
EarthChen <179153809@qq.com>
2018-01-23 19:56:48 +0800

    页面缓存

--- src/main/java/com/earthchen/seckill/controller/GoodsController.java
+++ src/main/java/com/earthchen/seckill/controller/GoodsController.java
@@ -2,17 +2,25 @@ package com.earthchen.seckill.controller;
 
 
 import com.earthchen.seckill.domain.SecKillUser;
+import com.earthchen.seckill.redis.GoodsKey;
 import com.earthchen.seckill.redis.RedisService;
 import com.earthchen.seckill.service.GoodsService;
 import com.earthchen.seckill.service.SecKillUserService;
 import com.earthchen.seckill.vo.GoodsVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -29,14 +37,49 @@ public class GoodsController {
     private GoodsService goodsService;
 
 
-    @GetMapping("/to_list")
-    public String list(Model model,
-                       SecKillUser user) {
-        List<GoodsVo> goodsVoList = goodsService.listGoodsVo();
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+
+    /**
+     * 使用页面级缓存
+     *
+     * @param request
+     * @param response
+     * @param model
+     * @param user
+     * @return
+     */
+    @GetMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Model model,
+                       SecKillUser user) {
         model.addAttribute("user", user);
-        model.addAttribute("goodsList", goodsVoList);
-        return "goods_list";
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        List<GoodsVo> goodsList = goodsService.listGoodsVo();
+        model.addAttribute("goodsList", goodsList);
+//    	 return "goods_list";
+        SpringWebContext ctx = new SpringWebContext(request,
+                response,
+                request.getServletContext(),
+                request.getLocale(),
+                model.asMap(),
+                applicationContext);
+        //手动渲染
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
     @GetMapping("/to_detail/{goodsId}")
@@ -66,6 +109,65 @@ public class GoodsController {
         model.addAttribute("miaoshaStatus", miaoshaStatus);
         model.addAttribute("remainSeconds", remainSeconds);
         return "goods_detail";
+    }
+
+
+    /**
+     * 使用url缓存(根据id)
+     * (实际上和页面缓存是一样的)
+     *
+     * @param request
+     * @param response
+     * @param model
+     * @param user
+     * @param goodsId
+     * @return
+     */
+    @RequestMapping(value = "/to_detail2/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String detail2(HttpServletRequest request,
+                          HttpServletResponse response,
+                          Model model,
+                          SecKillUser user,
+                          @PathVariable("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsDetail, "" + goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        //手动渲染
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if (now < startAt) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) {//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+//        return "goods_detail";
+
+        SpringWebContext ctx = new SpringWebContext(request, response,
+                request.getServletContext(), request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, "" + goodsId, html);
+        }
+        return html;
     }
 
 }
